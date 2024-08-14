29a63b54a2dd6790613eef4c94fda0ab7cd6c8b5
crystal512 <crystal.a512@gmail.com>
2024-08-13 20:28:45 -0400

    Data Table

--- src/test/java/playground/Activity1.java
+++ src/test/java/playground/Activity1.java
@@ -3,6 +3,9 @@ package playground;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 public class Activity1 {
 
@@ -21,7 +24,7 @@ public class Activity1 {
     @Test
     public void testFacebookTitle() {
         String applicationTitle = driver.getTitle();
-        Assert.assertEquals(applicationTitle, "xFacebook - log in or sign up");
+        Assert.assertEquals(applicationTitle, "Facebook - log in or sign up");
     }
 
     @AfterMethod
--- src/test/java/playground/MyFirstTest.java
+++ src/test/java/playground/MyFirstTest.java
@@ -2,7 +2,6 @@ package playground;
 
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class MyFirstTest {

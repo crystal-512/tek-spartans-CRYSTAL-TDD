ad4a2927823efcfa2ed64c56196f1ae06b0bbe55
crystal512 <crystal.a512@gmail.com>
2024-09-25 19:41:39 -0400

    fixed error

--- src/test/java/tek/tdd/base/BaseSetup.java
+++ src/test/java/tek/tdd/base/BaseSetup.java
@@ -35,7 +35,7 @@ public abstract class BaseSetup {
     public BaseSetup() {
         //Reading Config Files and Loading to properties
         String configFilePath = System.getProperty("user.dir")
-                + "/src/test/resources/configs/dev-config.properties";
+                + "/src/test/resources/config/dev-config.properties";
         try {
             LOGGER.debug("Reading Config file from path {}", configFilePath);
             InputStream inputStream = new FileInputStream(configFilePath);
